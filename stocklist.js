582bc2efbdc590eba4a9ee1b9e2f608a56e47d22
Mitchelle <54994430+Mitchelle-B@users.noreply.github.com>
2020-03-25 21:54:25 +0800

    Add files via upload

---
+++ stocklist.js
@@ -0,0 +1,10 @@
+//conservative  = < 0.7 beta score
+//balanced = 0.71 < beta < 1.5
+//aggressive = > 1.51
+
+
+var conservative = ["A2M.ax", "ABC.ax", "ABP.ax", "WBC.ax", "AGL.ax", "ALL.ax", "ANZ.ax", "CBA.ax", "NAB.ax", "SUN.ax", "FLT.ax", "CDA.ax","DTL.ax", "TNE.ax","CAR.ax","XPE.ax", "8C0.ax"]
+var balanced = ["BHP.ax","APT.ax","ALU.ax", "IFL.ax", "IFL.ax", "RIO.ax", "IRE.ax","WTC.ax","APX.ax", "PPH.ax", "OPL.ax", "SF1.ax", "TZL.ax", "RHP.ax", "SKO.ax", "PPS.ax", "LNU.ax"]
+var aggressive = ["QTG.ax", "AOI.ax", "SEI.ax", "QFY.ax", "CL1.ax", "EVS.ax", "SLX.ax", "EPD.ax", "NVX.ax", "CAG.ax", "SNS.ax", "DSE.ax", "NOV.ax", "NVH.ax", "K2F.ax", "MPW.ax"]
+
+var tickerarray = ["A2M.ax", "ABC.ax", "ABP.ax", "WBC.ax", "AGL.ax", "ALL.ax", "ANZ.ax", "CBA.ax", "NAB.ax", "SUN.ax", "FLT.ax", "CDA.ax","DTL.ax", "TNE.ax","CAR.ax","XPE.ax", "8C0.ax","BHP.ax","APT.ax","ALU.ax", "IFL.ax", "IFL.ax", "RIO.ax", "IRE.ax","WTC.ax","APX.ax", "PPH.ax", "OPL.ax", "SF1.ax", "TZL.ax", "RHP.ax", "SKO.ax", "PPS.ax", "LNU.ax","QTG.ax", "AOI.ax", "SEI.ax", "QFY.ax", "CL1.ax", "EVS.ax", "SLX.ax", "EPD.ax", "NVX.ax", "CAG.ax", "SNS.ax", "DSE.ax", "NOV.ax", "NVH.ax", "K2F.ax", "MPW.ax"]
