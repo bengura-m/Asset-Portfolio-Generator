079f15d65f26ea074bad942450ef621aac73fde4
Pete Brennan <pweda64@hotmail.com>
2020-03-25 21:41:07 +0800

    Added beta calculations

--- Assets/stockgenerator.js
+++ Assets/stockgenerator.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
-    $("#balanced").on("click", function() {
-      // hides main page
-      $("#main-page").attr("class", "hide");
-  
-      var tickerArray = ["fmg.ax", "wes.ax", "anz.ax", "csl.ax", "wow.ax"];
-  
-      for (var i = 0; i < tickerArray.length; i++) {
+  $(".appetite-btn").on("click", function() {
+    if (this.id === "conservative") {
+      betaRange = "conservative";
+    } else if (this.id === "balanced") {
+      betaRange = "balanced";
+    } else {
+      betaRange = "aggressive";
+    }
+
+    // hides main page
+    $("#main-page").attr("class", "hide");
+
+    var tickerArray = ["fmg.ax", "wes.ax", "anz.ax", "csl.ax", "wow.ax"];
+    // let conservativeArray = [];
+    // let balancedArray = [];
+    // let aggressiveArray = [];
+
+    // loops through our array and makes a number of api requests
+    for (let i = 0; i < tickerArray.length; i++) {
+      // if array has been stored in local storage we are going to use that - to save server requests (we have a limit) - could setup an || in the if statement (eg if older than 24 hours)
+      if (JSON.parse(localStorage.getItem("storedTickerArray0"))) {
+        let response = JSON.parse(
+          localStorage.getItem("storedTickerArray" + i)
+        );
+        displayStocks(response);
+      } else {
+        // yahoo finance api request settings
         var settings = {
           async: true,
           crossDomain: true,
@@ -14,27 +34,45 @@ $(document).ready(function() {
             tickerArray[i],
           method: "GET",
           headers: {
-            "Cache-Control": "max-age=2628000",
             "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-            "x-rapidapi-key": "43d993ffedmsh99ef2e1a86cfdf9p100046jsnaa81b3c2dd02"
+            "x-rapidapi-key":
+              "43d993ffedmsh99ef2e1a86cfdf9p100046jsnaa81b3c2dd02"
           }
         };
-  
         $.ajax(settings).done(function(response) {
-          console.log(response);
+          displayStocks(response);
+        });
+      }
+      function displayStocks(response) {
+        console.log(response);
+        localStorage.setItem("storedTickerArray" + i, JSON.stringify(response));
+        var beta = response.defaultKeyStatistics.beta.fmt;
+        if (beta < 0.7 && betaRange === "conservative") {
+          // conservativeArray.push(response)
+          // console.log(betaRange);
+          printStocks();
+        } else if (beta < 1.5 && beta > 0.7 && betaRange === "balanced") {
+          // balancedArray.push(response)
+          console.log(betaRange);
+          printStocks();
+        } else if (beta > 1.5 && betaRange === "aggressive") {
+          // aggressiveArray.push(response)
+          console.log(betaRange);
+          printStocks();
+        }
+
+        function printStocks() {
           var shareName = response.price.longName;
           var shareSymbol = response.price.symbol;
           var SharePrice = response.price.regularMarketPrice.fmt;
-          var beta = response.defaultKeyStatistics.beta.fmt;
           var sharePreviousClose = response.summaryDetail.previousClose.fmt;
           var SharePriceChange = (
             ((SharePrice - sharePreviousClose) / sharePreviousClose) *
             100
           ).toFixed(2);
-  
+
           var divTag = $("<div>");
           divTag.attr("class", "col 2 card-panel blue");
-  
           divTag.append(
             shareName,
             "<br>",
@@ -46,10 +84,9 @@ $(document).ready(function() {
             "%<br> Beta: ",
             beta
           );
-  
           $("#stocks").append(divTag);
-        });
+        }
       }
-    });
+    }
   });
-  
+});
