44d0bb45e55e173b7005fc09277629a976620662
Pete Brennan <pweda64@hotmail.com>
2020-03-27 08:19:41 +0800

    added news things to js file and integrated into html

--- Assets/stockgenerator.js
+++ Assets/stockgenerator.js
@@ -63,8 +63,9 @@ $(document).ready(function() {
             100
           ).toFixed(2);
 
-          var divTag = $("<div>");
-          divTag.attr("class", "col 2 card-panel blue");
+          // creates div tag and appends share details
+          var divTag = $("<button>");
+          divTag.attr("class", "stock col 2 card-panel blue");
           divTag.append(
             shareName,
             "<br>",
@@ -77,8 +78,138 @@ $(document).ready(function() {
             beta
           );
           $("#stocks").append(divTag);
+
+          // click listener for when a stock is clicked, then share name is passed to the news
+          $(document).on("click", ".stock", function() {
+            // when share button is clicked
+            $("#resultsSection").empty();
+            // shareName = $(this).html();
+            getNews(shareName);
+          });
         }
       }
     }
   });
+  // start of news section
+  // var shareName = "ASX News"; // default  = "ASX News"
+
+  function getNews(shareName) {
+    var searchTermQualifier = "";
+    var newsSourceList = [
+      "Fool.com.au",
+      "Australian Financial Review",
+      "Savings.com.au",
+      "Sbs.com",
+      "News.com.au",
+      "9news.com.au",
+      "Perthnow.com.au",
+      "abc-news-au",
+      "7news.com.au",
+      "Heraldsun.com.au"
+    ];
+    // NEWS API
+    var queryURL =
+      "https://newsapi.org/v2/everything?" + //api return everthing and not just headlines
+      "q=" +
+      shareName +
+      " " +
+      searchTermQualifier +
+      "&" +
+      "language=en&" + // language is english
+      "sortBy=publishedAt&" + // latest news first
+      "source=au&" + // Australian news sources only
+      "pageSize=30&" + // limit results to 30
+      "apiKey=87ce1bad23e546ef86515ee0e5175029";
+    var settings = {
+      url: queryURL,
+      method: "GET"
+    };
+
+    $.ajax(settings).done(function(response) {
+      var relevantResult = false;
+      var count = 0;
+
+      if (response.totalResults > 0) {
+        for (var i = 0; i < response.articles.length; i++) {
+          if (newsSourceList.includes(response.articles[i].source.name)) {
+            console.log(
+              newsSourceList.includes(response.articles[i].source.name)
+            );
+            relevantResult = true;
+            count++;
+            //GET title
+            var title = response.articles[i].title;
+            var titleEl = $("<a>").text(title);
+            var urlSource = response.articles[i].url;
+            titleEl.attr("href", urlSource);
+            titleEl.attr("target", "_blank");
+
+            //GET descripition
+            var description = response.articles[i].description;
+            var descriptionEl = $("<p>").text(description);
+
+            // GET news content snippet
+            var content = response.articles[i].content;
+            content = content.substring(0, 260); // truncate and limit to first 260 chars including spaces
+            var contentEl = $("<p>").text(content);
+
+            //GET Author
+            var author = response.articles[i].author;
+            var authorEl = $("<p>").text(author);
+
+            //GET publish date time
+            var publishedAt = response.articles[i].publishedAt;
+            publishedAt = new Date(publishedAt).toDateString(); // reformat date time
+            var publishedAtEl = $("<p>").text(publishedAt);
+
+            //GET image url
+            var urlImage = response.articles[i].urlToImage;
+            var imageEl = $("<img>").attr("src", urlImage);
+            imageEl.css(
+              //thumbnail styling
+              {
+                border: "1px solid #ddd" /* Gray border */,
+                "border-radius": "4px" /* Rounded border */,
+                padding: "5px" /* Some padding */,
+                margin: "5px" /* Some padding */,
+                width: "150px" /* Set a small width */
+              }
+            );
+
+            // DISPLAY HTML elements to DOM
+
+            $("#resultsSection").append(
+              imageEl,
+              titleEl,
+              contentEl,
+              authorEl,
+              publishedAtEl,
+              "<hr>"
+            );
+
+            if (count === 10) {
+              // limit relevant results to 10
+              break;
+            }
+          }
+        }
+        if (!relevantResult) {
+          var noNewsMessage = $("<p>").text(
+            "...Sorry, no relevant business news on '" +
+              shareName +
+              "' at the moment."
+          );
+          $("#resultsSection").append(noNewsMessage);
+          return;
+        }
+      } else {
+        var noNewsMessage = $("<p>").text(
+          "...Sorry, no latest business news on " +
+            shareName +
+            " at the moment."
+        );
+        $("#noNewsMessage").append(noNewsMessage);
+      }
+    }); // ajax API call close
+  } // getNews function close
 });
